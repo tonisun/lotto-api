816c3abb752689d377fe8ae57b89451bdfef6e09
Toni Zeidler <toni.z@gmx.de>
2022-12-03 22:36:57 +0100

    Lotto API Start

---
+++ src/test/java/de/egosanto/lottoapi/LottoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.egosanto.lottoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
