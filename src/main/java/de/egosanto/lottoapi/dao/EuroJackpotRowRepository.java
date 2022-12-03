94980380531624584bc6cfab4a4469f4a57af469
Toni Zeidler <toni.z@gmx.de>
2022-12-03 22:56:57 +0100

    Lotto API add entity and repo

---
+++ src/main/java/de/egosanto/lottoapi/dao/EuroJackpotRowRepository.java
@@ -0,0 +1,8 @@
+package de.egosanto.lottoapi.dao;
+
+
+import de.egosanto.lottoapi.entity.EuroJackpotRow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EuroJackpotRowRepository extends JpaRepository<EuroJackpotRow, Integer> {
+}
---
+++ src/main/java/de/egosanto/lottoapi/entity/EuroJackpotRow.java
@@ -0,0 +1,30 @@
+package de.egosanto.lottoapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "ej_lotto")
+public class EuroJackpotRow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "row_id")
+    private Integer id;
+
+    @Column(name = "n1")
+    private Integer number1;
+
+    @Column(name = "n2")
+    private Integer number2;
+
+    @Column(name = "n3")
+    private Integer number3;
+
+    @Column(name = "n4")
+    private Integer number4;
+
+    @Column(name = "n5")
+    private Integer number5;
+}
