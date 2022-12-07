d44b6eb930bd53bb7faa44d7ab480b2ab4ba0001
Toni Zeidler <toni.z@gmx.de>
2022-12-07 10:57:14 +0100

    Lotto API add EuroJackpotDrawRestConfigCRUD chng re

--- src/main/java/de/egosanto/lottoapi/config/EuroJackpotDrawRestConfigCRUD.java
+++ src/main/java/de/egosanto/lottoapi/config/EuroJackpotDrawRestConfigCRUD.java
@@ -1,7 +1,6 @@
 package de.egosanto.lottoapi.config;
 
 import de.egosanto.lottoapi.entity.EuroJackpotDraw;
-import de.egosanto.lottoapi.entity.EuroJackpotRow;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
